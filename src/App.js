5bb8cc5dd8594226aa3a229a10028ea8b3f9be32
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2022-11-15 19:54:42 +0100

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useFormik } from "formik";
+import { useEffect } from "react";
+import { Container, Input, Row, Col, Button } from "reactstrap";
+import "./App.css";
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      pliki: {},
+    },
+    //validate,
+    onSubmit: () => console.log(formik),
+  });
+
+  useEffect(() => {
+    if (formik.values.inputPliki) {
+      const reader = new FileReader();
+      let file = formik.values.inputPliki;
+      reader.onloadstart = () => {
+        formik.setFieldValue("pliki", {
+          loading: true,
+          file: undefined,
+          name: formik.values.inputPliki.name,
+        });
+      };
+      reader.onloadend = () => {
+        formik.setFieldValue("pliki", {
+          loading: false,
+          file: formik.values.inputPliki,
+          name: formik.values.inputPliki.name,
+        });
+      };
+      reader.readAsDataURL(reader.result);
+    }
+  }, [formik.values.inputPliki]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Row>
+          <Col mt={4} className='mt-4'>
+            <Input
+              type='file'
+              name='inputPliki'
+              id='inputPliki'
+              onChange={(e) => e.target.files[0]}
+            />
+            <Button type='Submit'>Wyślij</Button>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
